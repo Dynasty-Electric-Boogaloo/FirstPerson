3251f3ec9abe2fd4108f4976a68801dc5f102e17
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2025-03-10 14:49:54 +0100

    Update ClemMod.cs

--- Assets/ClemMod.cs
+++ Assets/ClemMod.cs
@@ -3,22 +3,33 @@ using UnityEditor;
 using UnityEngine;
 using System.Runtime.InteropServices;
 using System.Text;
+using UnityEditor.Overlays;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
 
 public class ClemMod : MonoBehaviour
 {
-
+    //En public pour pouvoir etre utilisé par d'autres scripts au cas où
+    public static bool lightMod;
+    
     [MenuItem("Tools/Clem Mod")]
     static void ChangeModSettings()
     {
         Debug.Log("Changing Mod Settings...");
+        lightMod = !lightMod; 
+        Debug.Log("Mod is now " + lightMod);
     }
-
+    
+    /// <summary>
+    /// Check si Clem est l'user et si oui passe directement en clem mod 
+    /// </summary>
     [InitializeOnLoad]
     static class CheckingUser
     {
         static CheckingUser()
         {
-            if (Environment.UserName == "p.farin") ChangeModSettings();
+            if (Environment.UserName == "c.lai") lightMod = true;
         }
     }
 }
+
