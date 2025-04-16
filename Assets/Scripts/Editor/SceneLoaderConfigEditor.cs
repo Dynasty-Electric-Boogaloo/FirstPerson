91120c8e4c75a4f70f9cc4713c14f6904c2f869d
Geek_Joystick <contact.geekjoystick@gmail.com>
2025-04-16 19:47:56 +0200

    Working on scene loading system

---
+++ Assets/Scripts/Editor/SceneLoaderConfigEditor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ScriptableObjects;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SceneLoaderConfig))]
+public class SceneLoaderConfigEditor : Editor
+{
+    SceneLoaderConfig t;
+    SerializedObject GetTarget;
+    SerializedProperty ThisList;
+    int ListSize;
+
+    private void OnEnable()
+    {
+        t = (SceneLoaderConfig)target;
+        GetTarget = new SerializedObject(t);
+        ThisList = GetTarget.FindProperty("sceneReferences");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        GetTarget.Update();
+        
+        GUILayout.Label("Scene References");
+        
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        ListSize = ThisList.arraySize;
+        ListSize = EditorGUILayout.IntField("Size", ListSize);
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/SceneLoaderConfig.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Scriptable Objects/Scene Loader Config", fileName = "Scene Loader Config")]
+    public class SceneLoaderConfig : ScriptableObject
+    {
+        [Serializable]
+        public struct SceneReference
+        {
+            public GUID logicSceneGuid;
+            public GUID blockoutSceneGuid;
+            public GUID decoSceneGuid;
+            public bool included;
+        }
+
+        public SceneReference[] sceneReferences;
+
+        private void OnValidate()
+        {
+            var editorBuildScenes = new List<EditorBuildSettingsScene>();
+
+            foreach (var sceneReference in sceneReferences)
+            {
+                if (!sceneReference.included)
+                    continue;
+
+                var sceneGuid = sceneReference.logicSceneGuid;
+                if (!sceneGuid.Empty())
+                {
+                    editorBuildScenes.Add(new EditorBuildSettingsScene(sceneGuid, true));
+                }
+                
+                sceneGuid = sceneReference.blockoutSceneGuid;
+                if (!sceneGuid.Empty())
+                {
+                    editorBuildScenes.Add(new EditorBuildSettingsScene(sceneGuid, true));
+                }
+                
+                sceneGuid = sceneReference.decoSceneGuid;
+                if (!sceneGuid.Empty())
+                {
+                    editorBuildScenes.Add(new EditorBuildSettingsScene(sceneGuid, true));
+                }
+            }
+
+            EditorBuildSettings.scenes = editorBuildScenes.ToArray();
+        }
+    }
+}
