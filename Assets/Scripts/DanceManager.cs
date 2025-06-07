b5343cd57df12553f68fa4b46b12f64618ed153d
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2025-06-08 01:48:08 +0200

    et c'est une fin de qte

--- Assets/Scripts/DanceManager.cs
+++ Assets/Scripts/DanceManager.cs
@@ -9,13 +9,33 @@ public class DanceManager : MonoBehaviour
     private static DanceManager _instance;
     [SerializeField] private List<MimicDestructionQte> qteMimic = new List<MimicDestructionQte>();
     [SerializeField] private List<MimicDestructionQte> qteMonstre = new List<MimicDestructionQte>();
+    [SerializeField] private float tolerance = 0.5f;
+    
     private MimicDestructionQte _currentMimicQTE;
     private PlayerDance _playerDance;
+    private bool qteIsPlaying;
+    private float timer;
+    private int currentIndex;
 
     private void Awake()
     {
         if (_instance == null)
             _instance = this;
+    }
+    
+    public bool GetIsInQte => qteIsPlaying;
+
+    private void Update()
+    {
+        if(!qteIsPlaying)
+            return;
+        
+        timer += Time.deltaTime;
+        
+        if(!Input.GetKeyDown(KeyCode.Space))
+            return;
+        
+        SetNextQte( timer > _currentMimicQTE.notes[currentIndex] - tolerance && timer < _currentMimicQTE.notes[currentIndex] + tolerance);
     }
 
     public static void StartQte(PlayerDance player, bool isMimic = true)
@@ -24,26 +44,36 @@ public class DanceManager : MonoBehaviour
             return;
         
         _instance._playerDance = player;
-        _instance.PlayQte(
-            isMimic
-                ? _instance.qteMimic[Random.Range(0, _instance.qteMimic.Count)]
-                : _instance.qteMonstre[Random.Range(0, _instance.qteMonstre.Count)], 0);
+        _instance._currentMimicQTE = isMimic
+            ? _instance.qteMimic[Random.Range(0, _instance.qteMimic.Count)]
+            : _instance.qteMonstre[Random.Range(0, _instance.qteMonstre.Count)];
+        
+        _instance.PlayQte( 0);
     }
 
-    private void PlayQte(MimicDestructionQte qte, int index)
+    private void PlayQte(int index)
     {
-       print( qte.notes[index]);
-       QteUiPanel.SetQte(qte.notes[index]);
-       if (false)
-       {
-           _playerDance.SetQteResult(false);
-           return;
-       }
-       
-       if(index < qte.notes.Count-1)
-           PlayQte(qte, index + 1);
-       else
-           _playerDance.SetQteResult(true);
+       currentIndex = index;
+       qteIsPlaying = true;
+       QteUiPanel.SetQte(_currentMimicQTE.notes[index]);
+    }
+
+    private void SetNextQte( bool isWon)
+    {
+        timer = 0;
+        if (!isWon)
+        {
+            _playerDance.SetQteResult(false);
+            _instance.qteIsPlaying = false;
+            return;
+        }
+        if(currentIndex < _currentMimicQTE.notes.Count-1)
+            PlayQte(currentIndex + 1);
+        else
+        {
+            _playerDance.SetQteResult(true);
+            _instance.qteIsPlaying = false;
+        }
     }
     
     [Serializable]
