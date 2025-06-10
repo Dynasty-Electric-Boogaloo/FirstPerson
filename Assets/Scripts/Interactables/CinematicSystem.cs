1d29abf755b34ccdae5e1bd54ad8daabb6f434d4
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2025-06-10 21:50:39 +0200

    cinematic update

--- Assets/Scripts/Interactables/CinematicSystem.cs
+++ Assets/Scripts/Interactables/CinematicSystem.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
+using UI;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class CinematicSystem : MonoBehaviour
 {
     public static CinematicSystem instance;
     [SerializeField] private List<VideoClip> clips;
+    [SerializeField] private RawImage image;
     private VideoPlayer _videoPlayer;
     private void Awake()
     {
@@ -16,20 +19,37 @@ public class CinematicSystem : MonoBehaviour
         _videoPlayer = GetComponent<VideoPlayer>()
             ? GetComponent<VideoPlayer>()
             : gameObject.AddComponent<VideoPlayer>();
+        _videoPlayer.loopPointReached += EndOfCinematic;
+        image.gameObject.SetActive(false);
     }
-    
+
+    private void EndOfCinematic(VideoPlayer source)
+    {
+        source.Stop();
+        image.gameObject.SetActive(false);
+        PauseManager.PauseGame(false, false);
+    }
+
     private void OnDestroy()
     {
         if (instance == this)
             instance = null;
     }
 
-    public static void PlayCinematic(int index = 0)
+    [ContextMenu("Testing cinematique")]
+    private void Testing()
+    {
+        PlayCinematic();
+    }
+
+    private static void PlayCinematic(int index = 0)
     {
         if(index > instance.clips.Count)
             return;
 
         instance._videoPlayer.clip = instance.clips[index];
         instance._videoPlayer.Play();
+        PauseManager.PauseGame(true, false);
+        instance.image.gameObject.SetActive(true);
     }
 }
