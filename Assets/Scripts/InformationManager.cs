5036d784affc533a2fd6a8c8d53e65518291aefa
Romsie <37375230+camilledardoize@users.noreply.github.com>
2025-06-13 19:18:44 +0200

    euh je résouds le conflit? j'ai peur

---
+++ Assets/Scripts/InformationManager.cs
@@ -0,0 +1,39 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class InformationManager : MonoBehaviour
+{
+   public static InformationManager Instance;
+   [SerializeField] private TMP_Text tmpText;
+
+   private void Awake()
+   {
+       if (Instance == null)
+           Instance = this;
+
+       if (Instance.tmpText)
+           tmpText.text = "";
+   }
+
+   private void OnDestroy()
+   {
+       if (Instance == this)
+           Instance = null;
+   }
+
+   public static void SetText(string text, float time)
+   {
+       if(!Instance || !Instance.tmpText)
+           return;
+
+       Instance.tmpText.text = text;
+       Instance.Invoke(nameof(GoBack), time);
+   }
+
+   //à clean
+   private void GoBack()
+   {
+       tmpText.text = "";
+   }
+}
