54dc899e2e372dec8e6ac0f744285d5841c11caa
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2025-06-13 22:14:49 +0200

    cinematique scripté

--- Assets/Scripts/Interactables/Events/EventRedLight.cs
+++ Assets/Scripts/Interactables/Events/EventRedLight.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Player;
 using UI;
 using UnityEngine;
 
 public class EventRedLight : EventObject
 {
-    [SerializeField] private Renderer wallClue;
+    [SerializeField] private SpriteRenderer wallClue;
     public override void DoEvent()
     {
         base.DoEvent();
+         if (GetComponentInChildren<MeshRenderer>())
+            GetComponentInChildren<MeshRenderer>().enabled = false;
+        if (TryGetComponent<BoxCollider>(out var box))
+            box.enabled = false;
+        
+        UiManager.SetEmpty();
+        
         PlayerRoot.SetRedLight(true);
         
         InformationManager.SetText("Press [F] to switch light", 2);
@@ -19,20 +27,30 @@ public class EventRedLight : EventObject
 
     private IEnumerator Event()
     {
+        
         yield return new WaitForSeconds(2.5f);
         UiManager.SetChaseBorder(true);
         //bruitFakeChase
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         //Son tapper la porte
-        yield return new WaitForSeconds(1f);
         
         if(wallClue)
+        {
             wallClue.gameObject.SetActive(true);
+            wallClue.DOFade(1, 1);
+        }
+        
+        yield return new WaitForSeconds(1f);
+        
+        gameObject.SetActive(false);
     }
 
     private void Start()
     {
-        if(wallClue)
-            wallClue.gameObject.SetActive(false);
+        if (!wallClue) 
+            return;
+        
+        wallClue.gameObject.SetActive(false);
+        wallClue.DOFade(0, 0);
     }
 }
