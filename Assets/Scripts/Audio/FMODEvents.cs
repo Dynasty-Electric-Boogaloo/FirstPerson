0750db0b299647f10b97f1709b85a070718a6d61
Sheep-Sinnesael <m.sinnesael@rubika-edu.com>
2025-06-16 20:32:15 +0200

    Update Vie

--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -14,15 +14,13 @@ public class FMODEvents : MonoBehaviour
     [field : SerializeField] public EventReference Spotted { get; private set;}
     [field : SerializeField] public EventReference Alert { get; private set;}
     [field : SerializeField] public EventReference Swap { get; private set;}
+    [field : SerializeField] public EventReference Death { get; private set;}
 
     public static EventReference GetStep() => instance ? instance.Step : new EventReference();
+    public static EventReference GetDeath() => instance ? instance.Death : new EventReference();
     public static EventReference GetAlert() => instance ? instance.Alert : new EventReference();
     public static EventReference GetSpotted() => instance ? instance.Spotted : new EventReference();
     public static EventReference GetSwap() => instance ? instance.Swap : new EventReference();
-
-
-
-    
     public static EventReference GetFlicker() => instance ? instance.Flicker : new EventReference();
 
     public static FMODEvents instance {get; private set;}
